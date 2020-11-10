8c048f82db592fd5b443dc73ba8e3b074f88b3d2
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-10 11:27:40 +0530

    Gaurav[ADDED]:Add five Random number and print their sum and average

--- SequencePracticeProblem1.1.js
+++ SequencePracticeProblem1.1.js
@@ -19,3 +19,15 @@
     let sum = dice1+dice2;
     console.log("Dice Roll Sum: "+sum);
 }
+
+//4
+{
+    let digit1 = Math.floor(Math.random()*100);  
+    let digit2 = Math.floor(Math.random()*100);
+    let digit3 = Math.floor(Math.random()*100);
+    let digit4 = Math.floor(Math.random()*100);
+    let digit5 = Math.floor(Math.random()*100);
+    let sum = digit1+digit2+digit3+digit4+digit5;
+    let avg = sum/5;
+    console.log("sum: "+ sum+ " avg: "+avg); 
+}
