30cd363f149b05a6ec7ca38ba343dd4a40f97f71
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-10 11:26:20 +0530

    Gaurav[ADDED]:Add two Random number and print their sum

--- SequencePracticeProblem1.1.js
+++ SequencePracticeProblem1.1.js
@@ -11,3 +11,11 @@
     let digit = Math.floor(Math.random()*10)%6+1;
     console.log("Dice Roll: "+digit);
 }
+
+//3
+{
+    let dice1 = Math.floor(Math.random()*10)%6+1;
+    let dice2 = Math.floor(Math.random()*10)%6+1;
+    let sum = dice1+dice2;
+    console.log("Dice Roll Sum: "+sum);
+}
